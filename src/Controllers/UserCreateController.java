a71333fa5c4ad86c7e3fbb5adc5b6703eaf5b5a9
Commando-Brando <branmuller@gmail.com>
2021-09-10 12:31:36 -0500

    added some basic fxml elements to the user reservation creation scene .java and fxml files

--- src/Controllers/UserCreateController.java
+++ src/Controllers/UserCreateController.java
@@ -1,3 +1,21 @@
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 // controls user reservation creation scene
-public class UserCreateController {
+public class UserCreateController implements Initializable {
+
+    @FXML
+    private Text mainmenuTV;
+
+    @FXML
+    private Text logoutTV;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
