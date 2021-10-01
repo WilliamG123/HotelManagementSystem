adf1d5ead4859ad4fef3c7eabc131917c8690f4c
Commando-Brando <branmuller@gmail.com>
2021-10-01 12:47:14 -0500

    added Hotel & Room model. Updated Reservation model

---
+++ src/Models/Room.java
@@ -0,0 +1,7 @@
+public class Room {
+    private int numRooms;
+    private double price;
+
+    // needs work not sure how to reconcile this with Reservations & Hotel models. Might need 2 different room models
+
+}
