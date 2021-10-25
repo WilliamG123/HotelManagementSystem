7e815d962a76f8ca052957dcc26f00660e39c7cb
Filiberto Rios <filibertoerios@gmail.com>
2021-10-24 21:21:48 -0500

    StaffUserCreate scene logic finished, not sure if database is ready for adding new entries to the tables; dynamically creates error message

--- src/main/Controllers/StaffCreateUserController.java
+++ src/main/Controllers/StaffCreateUserController.java
@@ -1,9 +1,13 @@
 import javafx.fxml.FXML;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDate;
+
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.control.ComboBox;
@@ -25,6 +29,7 @@ public class StaffCreateUserController {
     @FXML private PasswordField confPasswordField;
     @FXML private DatePicker dobPicker;
     @FXML private ComboBox<String> typePicker;
+    private StringBuffer errorMsg;
 
     @FXML private void handleTV(MouseEvent event){
         AnchorPane newScene = null;
@@ -46,6 +51,76 @@ public class StaffCreateUserController {
     }
 
     private void handleCreate(){
+        if(validateFields()){
+            errorLabel.setText("");
+            try {
+                String fname = firstNameField.getText();
+                String lname = lastNameField.getText();
+                String email = emailField.getText();
+                String pnum = phoneNumberField.getText();
+                String password = Hasher.getInstance("SHA-256").hash(passwordField.getText());
+                LocalDate dob = dobPicker.getValue();
+                String type = typePicker.getValue();
+                User user = new User(fname, lname, email, pnum, dob, password, type);
+                //User.Customer("add", user);
+                System.out.println(user);
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+        }else{
+            errorLabel.setText(errorMsg.toString());
+            errorLabel.setTextFill(Color.web("#FF0000"));
+        }
+    }
 
+    private boolean validateFields(){
+        boolean returnval = true;
+        errorMsg = new StringBuffer("Error:\n");
+        String fname = firstNameField.getText();
+        String lname = lastNameField.getText();
+        String password = passwordField.getText();
+        String confPassword = confPasswordField.getText();
+        String pnumber = phoneNumberField.getText();
+        String email = emailField.getText();
+        String type = typePicker.getValue();
+        LocalDate dob = dobPicker.getValue();
+        if(fname.equals("")){
+            errorMsg.append("--> Invalid First Name value\n");
+            returnval = false;
+        }
+        if(lname.equals("")){
+            errorMsg.append("--> Invalid Last Name value\n");
+            returnval = false;
+        }
+        if(email.equals("") || !Validators.isValidEmailAddress(email)){
+            errorMsg.append("--> Invalid Email value\n");
+            returnval = false;
+        }
+        if(pnumber.equals("") || !Validators.isValidPhoneNumber(pnumber)){
+            errorMsg.append("--> Invalid Phone Number value\n");
+            returnval = false;
+        }
+        if(password.equals("") || confPassword.equals("")){
+            errorMsg.append("--> Invalid Password value\n");
+        }
+        else if(!password.equals(confPassword)){
+            errorMsg.append("--> Passwords do not match\n");
+            returnval = false;
+        }
+        if(dob == null){
+            errorMsg.append("--> Invalid Date of Birth value\n");
+            returnval = false;
+        }
+        if(type == null || type.equals("")){
+            errorMsg.append("--> Invalid Type value\n");
+            returnval = false;
+        }
+        return returnval;
+    }
+
+    @FXML
+    void initialize(){
+        typePicker.getItems().add("CUST");
+        typePicker.getItems().add("EMP");
     }
 }
