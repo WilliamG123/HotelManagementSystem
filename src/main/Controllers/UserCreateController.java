2d11c460860314700bb36de5931aa717e496d0e2
rellik1453 <37806859+rellik1453@users.noreply.github.com>
2021-10-26 10:00:40 -0500

    Modified UserCreateController.java Added Type Ahead auto Complete of all hotels in search by name TextField.

--- src/main/Controllers/UserCreateController.java
+++ src/main/Controllers/UserCreateController.java
@@ -2,28 +2,38 @@ import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Side;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.TableColumn;
 
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import org.controlsfx.control.textfield.TextFields;
 
 import javax.swing.text.TabableView;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 // controls user reservation creation scene
@@ -44,10 +54,16 @@ public class UserCreateController extends DBConnection implements Initializable 
     private TableColumn<Hotels, Double> Price;
     @FXML
     private TableColumn<Hotels, Integer> amentities;
+    @FXML
+    private TextField tf_HotelName;
     //lets declare some variables
-
+    private ContextMenu entriesPopup;
     private Connection conn;
     private ObservableList<Hotels> list;
+
+    ArrayList<String> possibleWords = new ArrayList<String>();
+
+
 
     @FXML void sceneChange(MouseEvent event) {
         AnchorPane newScene = null;
@@ -72,13 +88,39 @@ public class UserCreateController extends DBConnection implements Initializable 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //adding all 10 required hotels for type ahead in search by hotel name 
+        possibleWords.add("The Magnolia All Suites");
+        possibleWords.add("The Lofts at Town Centre");
+        possibleWords.add("Park North Hotel");
+        possibleWords.add("The Courtyard Suites");
+        possibleWords.add("The Regency Rooms");
+        possibleWords.add("Town Inn Budget Rooms");
+        possibleWords.add("The Comfy Motel Place");
+        possibleWords.add("Sun Palace Inn");
+        possibleWords.add("HomeAway Inn");
+        possibleWords.add("Rio Inn");
 
+
+        TextFields.bindAutoCompletion(tf_HotelName, possibleWords);
         //run the method
         try {
             populateTableView();
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+        tf_HotelName.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                //Type Ahead auto complete
+                possibleWords.add(tf_HotelName.getText());
+                TextFields.bindAutoCompletion(tf_HotelName, possibleWords);
+
+                System.out.println(tf_HotelName.getText());
+
+            }
+        });
+
+
 
     }
 
@@ -117,9 +159,10 @@ public class UserCreateController extends DBConnection implements Initializable 
         //set data tp tableview
         tableHotels.setItems(list);
 
-
-
     }
+
+
+
 
 
 
