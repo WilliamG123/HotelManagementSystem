a0ccb67317da93b54d1926e9429c2f020384f8c6
rellik1453 <37806859+rellik1453@users.noreply.github.com>
2021-11-11 00:25:46 -0600

    updated QUERYS CLASS TESTING OUT RESULTS OF QUERYS CLASS CREATED QueryTestTableController and queryTestTable.fxml to test out queries class so far its looking good You will be able to call these functions as needed

--- src/main/Controllers/QueryTestTableController.java
+++ src/main/Controllers/QueryTestTableController.java
@@ -14,7 +14,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
-
+ 
 public class QueryTestTableController extends QUERYS implements Initializable {
     @FXML
     public Text bookBtn;
