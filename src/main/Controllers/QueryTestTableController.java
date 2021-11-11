4e0186170821eb305c33640d0b7d00bd165aa548
rellik1453 <37806859+rellik1453@users.noreply.github.com>
2021-11-11 00:24:01 -0600

    Changed DBConnection back to non-static and changed QUERIES.java back to NON static to fix issue when running.

---
+++ src/main/Controllers/QueryTestTableController.java
@@ -0,0 +1,89 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import org.controlsfx.control.textfield.TextFields;
+
+import java.net.URL;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class QueryTestTableController extends QUERYS implements Initializable {
+    @FXML
+    public Text bookBtn;
+    @FXML
+    public Button resetBtn;
+    @FXML
+    public Button btn_search;
+    @FXML
+    public TextField cityTF;
+    @FXML
+    private DatePicker checkinDP;
+    @FXML
+    private DatePicker checkoutDP;
+
+    @FXML
+    private ObservableList<Hotels> list;
+
+
+    public void populateTableView() throws SQLException, ClassNotFoundException {
+
+        //instantiate list
+        //both datepickers must have a value in order to run this query
+
+
+        Connection con = getConnection();
+        //PreparedStatement ps = con.prepareStatement("call hotel.getListAvailHotels(?)");
+        CallableStatement callableStatement = con.prepareCall("{call hotel.getListAvailHotels}");
+        ResultSet rs = callableStatement.executeQuery();
+        if (checkinDP.getValue() != null) {
+            System.out.println("DATE PICKERS HAVE VALUES!");
+            // check_in_date = checkinDP.getValue();
+            // check_out_date = checkinDP.getValue();
+            // ps.setDate(1, Date.valueOf(String.valueOf(check_in_date)));
+            // ps.setDate(2, Date.valueOf(String.valueOf(check_out_date)));
+
+        } else if (checkinDP.getValue() == null) {
+
+            System.out.println("DATEPICKERS NULL USE DEFUALT INSTEAD");
+
+            // long millis = System.currentTimeMillis();
+            // java.sql.Date date = new java.sql.Date(millis);
+            //ps.setDate(1, date);
+            // ps.setDate(2, date);
+        }
+
+
+
+        //loop through the resultSet , extract data and append it to our list
+        while (rs.next()) {
+
+            //Create a hotels Object , add data to it and finally append it to list
+            Hotels hotel = new Hotels();
+            hotel.setHotelname(rs.getString("hotel_name"));
+            hotel.setRooms(rs.getInt("hotel_availrms"));
+            hotel.setAmentities(rs.getInt("hotel_numofamend"));
+            hotel.setPrice(rs.getDouble("room_rate"));
+            hotel.setRating(rs.getInt("hotel_rating"));
+            hotel.setHoteladdr(rs.getString("hotel_address"));
+            hotel.setHoteldesc(rs.getString("hotel_desc"));
+
+            list.add(hotel);
+
+        }
+
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
--- src/main/java/QUERYS.java
+++ src/main/java/QUERYS.java
@@ -64,6 +64,74 @@ public class QUERYS extends DBConnection {
     }
 
 
+    public ResultSet getAmenitiesByHotelName(String hotelName) throws SQLException, ClassNotFoundException {
+        if(!hotelExist(hotelName)){
+           System.out.println("Hotel DOES NOT EXIST!");
+        }
+        //Declare statement, resultSet and CachedResultSet as null
+        //Statement stmt = null;
+        CallableStatement callableStatement = null;
+        ResultSet resultSet = null;
+        CachedRowSetImpl crs = null;
+        try {
+            //Connect to DB (Establish Oracle Connection)
+            Connection conn = getConnection();
+            //Callable statement is an extension of prepared statement so it prevents against SQL INJECTION
+            callableStatement = conn.prepareCall("{CALL getAmenitiesByHotel(?)}");
+            callableStatement.setString(1, hotelName);
+
+            //Execute select (query) operation
+            resultSet = callableStatement.executeQuery();
+            //CachedRowSet Implementation
+            //In order to prevent "java.sql.SQLRecoverableException: Closed Connection: next" error
+            //We are using CachedRowSet
+            crs = new CachedRowSetImpl();
+            crs.populate(resultSet);
+        } catch (SQLException e) {
+            System.out.println("Problem occurred at executeQuery operation : " + e);
+            throw e;
+        } finally {
+            if (resultSet != null) {
+                //Close resultSet
+                resultSet.close();
+            }
+            if (callableStatement != null) {
+                //Close Statement
+                callableStatement.close();
+            }
+            //Close connection
+            getConnection().close();
+        }
+        //Return CachedRowSet
+        return crs;
+    }
+
+
+
+    public boolean hotelExist(String hotelName) throws SQLException, ClassNotFoundException {
+        CallableStatement callableStatement = null;
+        ResultSet resultSet = null;
+        try {
+            Connection conn = getConnection();
+            callableStatement = conn.prepareCall("{CALL doesHotelExist(?)}");
+            callableStatement.setString(1, hotelName);
+            resultSet = callableStatement.executeQuery();
+            if( resultSet.getInt(1)==0)
+                return false;
+        } catch (SQLException e) {
+            System.out.println("Problem occurred at executeQuery operation : " + e);
+            throw e;
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (callableStatement != null) {
+                callableStatement.close();
+            }
+            getConnection().close();
+        }
+        return true;
+    }
 
     public void dbExecuteUpdate(String sqlStmt) throws SQLException, ClassNotFoundException {
         //Declare statement as null
