53968b26864a5d26da1a0ea5880bf03d0b3f7fea
Commando-Brando <branmuller@gmail.com>
2021-11-18 08:46:47 -0600

    added query calls

--- src/main/Controllers/SharedBooking.java
+++ src/main/Controllers/SharedBooking.java
@@ -15,10 +15,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
-public class SharedBooking implements Initializable {
+public class SharedBooking extends DBConnection implements Initializable {
 
     @FXML private Text hNameTF;
     @FXML private Text hAddressTF;
@@ -42,7 +44,7 @@ public class SharedBooking implements Initializable {
     @FXML private ListView<String> amenitiesLV;
     @FXML private ObservableList<Room> roomsList;
 
-    private Property property;
+    private Hotels hotel;
     private String accountType;
 
     // TODO: 11/17/2021 write handler method for the book button
@@ -52,32 +54,55 @@ public class SharedBooking implements Initializable {
     @FXML void addRoom(ActionEvent event) { }
 
 
-    /*public SharedBooking(Property property, String accountType) {
-        this.property = property;
+    public SharedBooking(Hotels hotel, String accountType) {
+        this.hotel = hotel;
         this.accountType = accountType;
-    }*/
-
-    @FXML void sceneChange(MouseEvent event) {
-        AnchorPane newScene = null;
-        try{
-            if(event.getSource() == returnTF){
-                newScene = FXMLLoader.load(getClass().getResource("UserCreate.fxml"));
-            }
-        }catch(IOException e){
-            e.printStackTrace();
-        }
-        Scene scene = new Scene(newScene);
-        Stage window = (Stage)((Node) event.getSource()).getScene().getWindow();
-        window.setScene(scene);
-        window.show();
     }
 
-    public void populateRoomTable() {
-        // TODO: 11/17/2021 write query stuff to get each room type, their price, and their available amount of rooms per dates
+    public List<String> getRoomTypes() throws ClassNotFoundException, SQLException {
+        List<String> roomsTypes = new ArrayList<>();
+        Connection con = null;
+        con = getConnection();
+        CallableStatement callableStatement = con.prepareCall("{call hotel.getAllRoomTypes(?)}");
+        callableStatement.setString(1, hotel.getHotelId());
+        ResultSet rs = callableStatement.executeQuery();
+
+        //loop through the resultSet & add each room type to the ArrayList
+        while (rs.next()) {
+            roomsTypes.add(rs.getString("type_name"));
+        }
+        return roomsTypes;
+    }
+
+    public void populateRoomTable() throws ClassNotFoundException, SQLException {
+        // TODO: 11/18/2021 write a way to dynamically query the info
+
+        Connection con = null;
+        con = getConnection();
+        CallableStatement callableStatement = con.prepareCall("{call hotel.getRoomByType(?, ?)}");
+        callableStatement.setString(1, hotel.getHotelId());
+        ResultSet rs = callableStatement.executeQuery();
+
+        //loop through the resultSet & add each room type to the ArrayList
+        while (rs.next()) {
+        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        try {
+            populateAmenitiesList();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        hNameTF.setText(this.hotel.getHotelname());
+        hAddressTF.setText(this.hotel.getHoteladdr());
+        hRatingTF.setText(String.valueOf(this.hotel.getRating()) + "/10 Stars");
+        descriptionTF.setText(hotel.getHoteldesc());
 
         // TODO: 11/17/2021 delete/move this stuff to a populate TableView method
         roomsList = FXCollections.observableArrayList();
@@ -93,6 +118,34 @@ public class SharedBooking implements Initializable {
         priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
 
         roomTB.setItems(roomsList);
+    }
 
+    public void populateAmenitiesList() throws ClassNotFoundException, SQLException {
+        Connection con = null;
+        con = getConnection();
+        CallableStatement callableStatement = con.prepareCall("{call getAmenitiesByHotel(?)}");
+        callableStatement.setString(1, hotel.getHotelname());
+        ResultSet rs = callableStatement.executeQuery();
+
+        //loop through the resultSet & add each amenity to the ListView
+        while(rs.next()) {
+            amenitiesLV.getItems().add(rs.getString("Amenities_desc"));
+        }
+
+    }
+
+    @FXML void sceneChange(MouseEvent event) {
+        AnchorPane newScene = null;
+        try{
+            if(event.getSource() == returnTF){
+                newScene = FXMLLoader.load(getClass().getResource("UserCreate.fxml"));
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(newScene);
+        Stage window = (Stage)((Node) event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
     }
 }
