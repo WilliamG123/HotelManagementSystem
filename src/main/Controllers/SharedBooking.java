758859726a19afa1302cf65715776b800591d7b7
Commando-Brando <branmuller@gmail.com>
2021-11-18 23:28:15 -0600

    Added cart functionality

--- src/main/Controllers/SharedBooking.java
+++ src/main/Controllers/SharedBooking.java
@@ -16,11 +16,14 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class SharedBooking extends DBConnection implements Initializable {
+
+    public static final String REMOVEFROMCART = "removefromcart";
+    public static final String ADDTOCART = "addtocart";
+    public static final int ERROR = -1;
+    public static final int SUCCESS = 0;
 
     @FXML private Text hNameTF;
     @FXML private Text hAddressTF;
@@ -37,22 +40,21 @@ public class SharedBooking extends DBConnection implements Initializable {
     @FXML private Button bookBtn;
     @FXML private Button removeRoomBtn1;
     // rooms available table
-    @FXML private TableView<Room> roomTB;
+    @FXML private TableView<Room> roomTV;
     @FXML private TableColumn<Room, String> styleColumn;
-// TODO: 11/17/2021 make occupancy column
-    @FXML private TableColumn<Room, String> occupancyColumn;
     @FXML private TableColumn<Room, String> availableColumn;
     @FXML private TableColumn<Room, String> priceColumn;
     // cart variables
     @FXML private TableView<Room> cartTV;
-    @FXML private TableColumn<?, ?> style2Column;
-    @FXML private TableColumn<?, ?> amount2Column;
-    @FXML private TableColumn<?, ?> price2Column;
+    @FXML private TableColumn<Room, String> style2Column;
+    @FXML private TableColumn<Room, String> amount2Column;
+    @FXML private TableColumn<Room, String> price2Column;
 
     @FXML private ListView<String> amenitiesLV;
     @FXML private ObservableList<Room> roomsList;
+    @FXML private ObservableList<Room> roomTypes;
+    @FXML private ObservableList<Room> cartList;
     @FXML private ChoiceBox<String> roomCB;
-
     private Hotels hotel;
     private String accountType;
 
@@ -61,19 +63,90 @@ public class SharedBooking extends DBConnection implements Initializable {
 
     @FXML void roomChange(ActionEvent event) {
         Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow(); // for displaying Toast error messages
-        // TODO: 11/18/2021 write fucntionality for adding a room to the cart
         String choice;
 
-        if(event.getSource() == addRoomBtn) {
-            choice = roomCB.getValue();
-            if(choice == null){
-                Toast.makeText(stage, "Error: no room selected", 2000, 500, 500);
-                return;
-            }
-
+        choice = roomCB.getValue();
+        if(choice == null){
+            Toast.makeText(stage, "Error: no room selected", 2000, 500, 500);
+            return;
         }
 
-        // TODO: 11/18/2021 write functionality for removing a room from the cart
+        // find the room type in the list to be decremented
+        for(int i = 0; i < roomTypes.size(); i++) {
+            if(roomTypes.get(i).getType().equals(choice)) {
+                if(event.getSource() == addRoomBtn) {
+                    cartHandler(roomTypes.get(i), ADDTOCART, event);
+                } else {
+                    cartHandler(roomTypes.get(i), REMOVEFROMCART, event);
+                }
+            }
+        }
+    }
+
+    private int roomsHandler(Room room, String action, ActionEvent event) {
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+
+        if(action.equals(ADDTOCART)){
+            for(int i = 0; i < roomTypes.size(); i++){
+                if(room.getType().equals(roomTypes.get(i).getType())) {
+                    if(roomTypes.get(i).getAmountAvailable() == 0) {
+                        Toast.makeText(stage, "Error: there are no more " + room.getType() + " rooms available", 2000, 500, 500);
+                        return ERROR;
+                    }
+                    roomTypes.get(i).setAmountAvailable(roomTypes.get(i).getAmountAvailable() - 1);
+                    roomTV.refresh();
+                    return SUCCESS;
+                }
+            }
+        } else {
+            for (int i = 0; i < roomTypes.size(); i++) {
+                if (room.getType().equals(roomTypes.get(i).getType())) {
+                    roomTypes.get(i).setAmountAvailable(roomTypes.get(i).getAmountAvailable() + 1);
+                    roomTV.refresh();
+                    return SUCCESS;
+                }
+            }
+        }
+        return ERROR;
+    }
+
+    private void cartHandler(Room room, String action, ActionEvent event) {
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        boolean exists = false;
+
+        if(action.equals(REMOVEFROMCART)){
+            for(int i = 0; i < cartList.size(); i++) {
+                if(room.getType().equals(cartList.get(i).getType())) {
+                    exists = true;
+                    cartList.get(i).setAmountAvailable(cartList.get(i).getAmountAvailable() - 1);
+                    if(cartList.get(i).getAmountAvailable() == 0) {
+                        cartList.remove(cartList.get(i));
+                    }
+                }
+            }
+            if(!exists){
+                Toast.makeText(stage, "Error: there is no " + room.getType() + " room in your cart", 2000, 500, 500);
+                return;
+            }
+            roomsHandler(room, REMOVEFROMCART, event);
+        } else {
+            if(roomsHandler(room, ADDTOCART, event) == ERROR){
+                return;
+            } else {
+                for(int i = 0; i < cartList.size(); i++) {
+                    if(room.getType().equals(cartList.get(i).getType())) {
+                        cartList.get(i).setAmountAvailable(cartList.get(i).getAmountAvailable() + 1);
+                        exists = true;
+                        break;
+                    }
+                }
+                if(!exists) {
+                    Room r = new Room(room.getPrice(), room.getType(), 1);
+                    cartList.add(r);
+                }
+            }
+        }
+        cartTV.refresh();
     }
 
     public SharedBooking(Hotels hotel, String accountType) {
@@ -102,7 +175,7 @@ public class SharedBooking extends DBConnection implements Initializable {
         con = getConnection();
         CallableStatement callableStatement = con.prepareCall("{call hotel.getRoomByType(?, ?)}");
 
-        ObservableList<Room> roomTypes = getRoomTypes();
+        roomTypes = getRoomTypes();
         ObservableList<String> typeStrings = FXCollections.observableArrayList();
 
         for(Room r : roomTypes) {
@@ -110,35 +183,25 @@ public class SharedBooking extends DBConnection implements Initializable {
             typeStrings.add(r.getType());
         }
 
-        for(Room room : roomTypes) {
-            callableStatement.setString(1, room.getType());
+        roomCB.setItems(typeStrings); // set room types available for the choice box
+
+        for(int i = 0; i < roomTypes.size(); i++) {
+            callableStatement.setString(1, roomTypes.get(i).getType());
             callableStatement.setString(2, hotel.getHotelId());
             ResultSet rs = callableStatement.executeQuery();
 
-            ResultSetMetaData rsmd = rs.getMetaData();
-            int columnsNumber = rsmd.getColumnCount();
-            while (rs.next()) {
-                for (int i = 1; i <= columnsNumber; i++) {
-                    if (i > 1) System.out.print(",  ");
-                    String columnValue = rs.getString(i);
-                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
-                }
-                System.out.println("");
-            }
-            //room.setPrice(rs.getDouble("room_rate"));
-            //room.setAmountAvailable("");
-
+            rs.next();
+            roomTypes.get(i).setAmountAvailable(rs.getInt("count(*)"));
+            roomTypes.get(i).setPrice(rs.getDouble("room_rate"));
         }
-
-        roomCB.setItems(typeStrings);
-
-        //loop through the resultSet & add each room type to the ArrayList
-        //while (rs.next()) {
-        //}
+        roomTV.setItems(roomTypes);
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        cartList = FXCollections.observableArrayList();
+        cartTV.setItems(cartList);
+
         amenitiesLV.setMouseTransparent(true);
         amenitiesLV.setFocusTraversable(false);
         cartTV.setMouseTransparent(true);
@@ -148,6 +211,16 @@ public class SharedBooking extends DBConnection implements Initializable {
         hAddressTF.setText(this.hotel.getHoteladdr());
         hRatingTF.setText(String.valueOf(this.hotel.getRating()) + "/10 Stars");
         descriptionTF.setText(hotel.getHoteldesc());
+
+        // set the columns up for the rooms available TableView
+        styleColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+        availableColumn.setCellValueFactory(new PropertyValueFactory<>("amountAvailable"));
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        // set the columns up for the Cart TableView
+        style2Column.setCellValueFactory(new PropertyValueFactory<>("type"));
+        amount2Column.setCellValueFactory(new PropertyValueFactory<>("amountAvailable"));
+        price2Column.setCellValueFactory(new PropertyValueFactory<>("price"));
 
         try {
             populateAmenitiesList();
@@ -170,7 +243,6 @@ public class SharedBooking extends DBConnection implements Initializable {
         while(rs.next()) {
             amenitiesLV.getItems().add(rs.getString("Amenities_desc"));
         }
-
     }
 
     @FXML void sceneChange(MouseEvent event) {
