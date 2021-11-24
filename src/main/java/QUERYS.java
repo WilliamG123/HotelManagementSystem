55f1bf1df7d95e4f6d68b5302e18018198ab5c75
rellik1453 <37806859+rellik1453@users.noreply.github.com>
2021-11-23 18:48:01 -0600

    SessionSingleton created for SharedBooking.java Changed start up scene to user create and returns to login if Singleton User == null

--- src/main/java/QUERYS.java
+++ src/main/java/QUERYS.java
@@ -185,7 +185,52 @@ public class QUERYS {
 
 
 
-//use: returns result set for total amenities for a given hotel name. i.e "The Magnolia All Suites"
+    //use: returns result set for total amenities for a given hotel name. i.e "The Magnolia All Suites"
+    public ResultSet getCountOfAvailRoomsByDateRange(String RoomType, int hotelID, LocalDate checkin , LocalDate checkout) throws SQLException, ClassNotFoundException {
+        // if(!hotelExist(hotelName)){
+        //   System.out.println("Hotel DOES NOT EXIST!");
+        // }
+        //Declare statement, resultSet and CachedResultSet as null
+        //Statement stmt = null;
+        CallableStatement callableStatement = null;
+        ResultSet resultSet = null;
+        CachedRowSetImpl crs = null;
+        try {
+            //Connect to DB (Establish Oracle Connection)
+            dbConnect();
+            //Callable statement is an extension of prepared statement so it prevents against SQL INJECTION
+            callableStatement = conn.prepareCall("{CALL getAmenitiesByHotel(?)}");
+            callableStatement.setString(1, hotelName);
+
+            //Execute select (query) operation
+            resultSet = callableStatement.executeQuery();
+            //CachedRowSet Implementation
+            //In order to prevent "java.sql.SQLRecoverableException: Closed Connection: next" error
+            //We are using CachedRowSet
+            crs = new CachedRowSetImpl();
+            crs.populate(resultSet);
+        } catch (SQLException e) {
+            System.out.println("Problem occurred at executeQuery operation : " + e);
+            throw e;
+        } finally {
+            if (resultSet != null) {
+                //Close resultSet
+                resultSet.close();
+            }
+            if (callableStatement != null) {
+                //Close Statement
+                callableStatement.close();
+            }
+            //Close connection
+            conn.close();
+        }
+        //Return CachedRowSet
+        return crs;
+    }
+
+
+
+    //use: returns result set for total amenities for a given hotel name. i.e "The Magnolia All Suites"
     public ResultSet getAmenitiesByHotelName(String hotelName) throws SQLException, ClassNotFoundException {
        // if(!hotelExist(hotelName)){
         //   System.out.println("Hotel DOES NOT EXIST!");
